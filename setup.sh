fffc72172fd78d20bfa8b9747b652868037e4908
richardyantas5 <richadyantas5@gmail.com>
2020-12-20 11:58:03 -0500

    external json was added

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
+# git submodule add https://github.com/open-source-parsers/jsoncpp lib/jsoncpp
+# git remote add origin https://github.com/richardyantas/uppaalLibrary
+# git remote remove origin
+# git remote add origin https://github.com/richardyantas/uppaalLibrary.gitg
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/serendipita/Documents/uppaalLibrary/build/lib/
